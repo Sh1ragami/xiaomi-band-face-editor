e3499146cd2a056a4e736a4252c02ba885fc25c1
白神晴陽 <aoshi09275398@icloud.com>
2026-01-20 06:36:38 +0900

    add: レスポンシブデザイン対応

--- src/shared/icons.tsx
+++ src/shared/icons.tsx
@@ -141,3 +141,11 @@ export const CloseIcon: React.FC<IconProps> = ({ className }) => (
     <line x1="6" y1="6" x2="18" y2="18" />
   </svg>
 );
+
+export const MoreIcon: React.FC<IconProps> = ({ className }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+    <circle cx="5" cy="12" r="1.5" fill="currentColor" stroke="none" />
+    <circle cx="12" cy="12" r="1.5" fill="currentColor" stroke="none" />
+    <circle cx="19" cy="12" r="1.5" fill="currentColor" stroke="none" />
+  </svg>
+);
